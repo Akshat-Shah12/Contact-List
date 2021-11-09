d030fd06d01ca7448e15619e42bc9e18d7a4eb2a
Akshat Shah <avshah2001@gmail.com>
2021-11-09 17:05:08 +0530

    router issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "tailwindcss/tailwind.css";
 import "./App.css";
-<<<<<<< HEAD
 import { uuid } from "uuidv4";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Switch, Route } from "react-router-dom";
-=======
-import React, { useState } from "react";
->>>>>>> parent of c5fb7bd (Deletion possible from the list)
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import ContactCard from "./components/ContactCard";
 import Router from "./Router";
 function App() {
-<<<<<<< HEAD
   // const [contacts, setcontacts] = useState([]);
   // const LOCAL_STORAGE_KEY = "contacts";
   // // const contacts=[
@@ -75,34 +70,6 @@ function App() {
     //     {/* <ContactCard /> */}
     //   </BrowserRouter>
     // </div>
-=======
-  const [contacts, setcontacts] = useState([]);
-  // const contacts=[
-  //   {
-  //     id:"1",
-  //     name:"Akshat Shah",
-  //     email:"adknd@gmail.com",
-  //   },
-  //   {
-  //     id:"2",
-  //     name:"Kknd Shah",
-  //     email:"dd@gmail.com",
-  //   }
-  // ];
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setcontacts([...contacts, contact]);
-  };
-  return (
-    <>
-      <Header />
-      <br />
-      <AddContact addContactHandler={addContactHandler} />
-      <br />
-      <ContactList contacts={contacts} />
-      <ContactCard />
-    </>
->>>>>>> parent of c5fb7bd (Deletion possible from the list)
   );
 }
 
